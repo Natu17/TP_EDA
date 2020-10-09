2f977750a269b0a5696427941d85c5b0b01ff91a
Azul <kjyazul@gmail.com>
2020-10-09 18:02:16 -0300

    Method search modified and test for search added

--- TP_EDA/src/test/java/IndexTest.java
+++ TP_EDA/src/test/java/IndexTest.java
@@ -1,6 +1,9 @@
+import model.PlaceLocation;
 import org.apache.commons.csv.CSVRecord;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 public class IndexTest {
     Index lv = new Index();
@@ -33,6 +36,37 @@ public class IndexTest {
     void inequality() {
         String str1 = "algoritmos", str2 = "arquitectura", str3 = "itba";
         Assertions.assertTrue(( lv.distance(str1, str2) +  lv.distance(str2, str3)) >=  lv.distance(str1, str3));
+    }
+
+    @Test
+    void firstResultCorrect(){
+        lv.put("hospital aleman", new PlaceLocation("hospital aleman", -34.591806, -58.402139));
+        lv.put("hospital alemania", new PlaceLocation("hospital alemania", -34.6543773, -58.5324));
+        lv.put("hospital italiano", new PlaceLocation("hospital italiano", -34.606583, -58.425));
+        lv.put("hospital italienne", new PlaceLocation("hospital italienne", -34.604545, -58.44357));
+        lv.put("hospital español", new PlaceLocation("hospital español", -34.654764, -58.32525));
+        lv.put("hospital españa", new PlaceLocation("hospital españa", -34.654567, -58.323));
+
+        List<PlaceLocation> result = lv.search("hospital italiano");
+
+        Assertions.assertEquals("hospital italiano", result.get(0).getName());
+        Assertions.assertEquals("hospital italienne", result.get(1).getName());
+
+        result = lv.search("hospital aleman");
+
+        Assertions.assertEquals("hospital aleman", result.get(0).getName());
+        Assertions.assertEquals("hospital alemania", result.get(1).getName());
+
+        result = lv.search("hospital español");
+
+        Assertions.assertEquals("hospital español", result.get(0).getName());
+        Assertions.assertEquals("hospital españa", result.get(1).getName());
+
+        //Busco un lugar mal escrito
+        result = lv.search("hospital espanol");
+
+        Assertions.assertEquals("hospital español", result.get(0).getName());
+        Assertions.assertEquals("hospital españa", result.get(1).getName());
     }
 
 }
